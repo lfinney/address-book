01b93dcbb0821f27f50554edbd6a2403a9135b13
Luke Finney <lucaswfinney@gmail.com>
2018-02-01 11:32:43 -0700

    Setup static assets

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return 'This is the homepage'
 
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("profile.html", name=name)
+
 if __name__ == "__main__":
     app.run(debug=True)
